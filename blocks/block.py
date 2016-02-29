251175becd91183fb42590d80acc7850893657d3
BuildTools <RomaineRC@gmail.com>
2016-02-29 15:53:46 +0000

    Made a decent amount of progress with both of these files, need to iron out bugs in the resource pack loader and add a lot more features to the blockclass

--- blocks/block.py
+++ blocks/block.py
@@ -1,6 +1,6 @@
 import pickle
-from enum import Enum
 import bpy
+from enum import Enum
 
 
 class Block():
@@ -10,60 +10,116 @@ class Block():
     preview_location = (0, 0, 1.01)
 
     def __init__(self):
-        self.name = ""
-        self.colour = ()
-        self.location = ()
-        self.defs = {}
+        name = ""
+        colour = ()
+        location = ()
+        defs = {}
         self.load_defs()
-        self.construct = None
+        construct = None
+        mesh = None
 
     def load_defs(self):
         """Loads the definitions of Minecaft block data values."""
         with open("blocks.pkl", "rb") as f:
             unpickle = pickle.Unpickler(f)
-            defs = unpickle.load()
-            self.defs = defs
+            self.defs = unpickle.load()
 
     def create_block(self, dvs, location=preview_location):
         "Create a block; anything that has an int location rather than float"
         self.name = self.find_name(dvs)
-        self.location = location
+        location = location
 
-        if "slab" in self.name:
-            ConstructType.half
-        elif "flower" in self.name:
-            ConstructType.cross
-        else:
-            ConstructType.block
+        # if "slab" in name:
+        #    ConstructType.half
+        # elif "flower" in name:
+        #    ConstructType.cross
+        # else:
+        #    ConstructType.block
 
-    def find_textures():
-        bpy.data.images.
+        Material.diffuse(self, self.name)
+
+    def uv_textures():
+        return name + ".png"
 
     def find_name(self, dvs):
         dv1, dv2 = dvs
         return self.defs[dv1][dv2][0]
 
 
-class material(Enum):
+class Material():
 
     """Not yet implimented I'm not sure if its necessary."""
 
-    wood = None
-    stone = None
-    wool = None
-    glass = None
+    group = bpy.data.node_groups.new(type="ShaderNodeTree", name="diffuse")
+
+    group.inputs.new("NodeSocketColor", "Image")
+    group.inputs.new("NodeSocketColor", "Alpha")
+    input_node = group.nodes.new("NodeGroupInput")
+
+    group.outputs.new("ShaderNodeBsdfDiffuse", "Out")
+    output_node = group.nodes.new("NodeGroupOutput")
+
+    diffuse = group.nodes.new(type="ShaderNodeBsdfDiffuse")
+    trans = group.nodes.new(type="ShaderNodeBsdfTransparent")
+    mix = group.nodes.new(type="ShaderNodeMixShader")
+    mat = group.nodes.new(type="ShaderNodeOutputMaterial")
+
+    group.links.new(input_node.outputs["Image"], diffuse.inputs[0])
+
+    group.links.new(input_node.outputs["Alpha"], mix.inputs[0])
+
+    group.links.new(diffuse.outputs[0], mix.inputs[2])
+
+    group.links.new(trans.outputs[0], mix.inputs[1])
+
+    group.links.new(mix.outputs[0], output_node.inputs[0])
+    group.links.new(mix.outputs[0], mat.inputs[0])
+
+    def diffuse(self, name):
+        mat = bpy.data.materials.new(name)
+        mat.use_nodes = True
+        diffuse = mat.node_tree.nodes['Diffuse BSDF']
+        group_node = mat.node_tree.nodes.new("ShaderNodeGroup")
+        group_node.node_tree = Material.group
+        tex = mat.node_tree.nodes.new(type="ShaderNodeTexImage")
+        tex.image = bpy.data.images["stone_" +
+                                    self.name.replace(" ", "_").lower() +
+                                    ".png"]
+        mat.node_tree.links.new(tex.outputs[0], group_node.inputs["Image"])
+        mat.node_tree.links.new(tex.outputs[1], group_node.inputs["Alpha"])
+
+    # wood = None
+    # stone = None
+    # glass = None
 
 
 class ConstructType(Enum):
 
     def _block():
-            bpy.ops.mesh.primitive_cube_add(radius=1,
-                                            view_align=False,
-                                            enter_editmode=False,
-                                            location=Block.preview_location)
+        bpy.ops.mesh.primitive_cube_add(radius=1,
+                                        view_align=False,
+                                        enter_editmode=False,
+                                        location=Block.preview_location)
+
+    def _cross():
+        bpy.ops.mesh.primitive_plane_add(raduis=1,
+                                         view_align=False,
+                                         location=Block.preview_location)
+
+    def _cross():
+        pass
+
+    def _stairs():
+        pass
+
+    def _fence():
+        pass
 
     block = _block()
+    cross = _cross()
 
+    fence = _fence()
+    stairs = _stairs()
     # cross
     # fence
     # stairs
--- blocks/resourcepacks.py
+++ blocks/resourcepacks.py
@@ -1,31 +1,72 @@
-from zipfile import ZipFile
-import os
+"""Handles the loading of resource packs."""
+
 import bpy
+import os
+import sys
 
+from os import path
+from zipfile import ZipFile
 
-def load_textures(archive=None):
+if sys.platform == 'darwin':
+    MCPATH = os.path.join(
+        os.environ['HOME'], 'Library', 'Application Support', 'minecraft')
+elif sys.platform == 'linux':
     MCPATH = os.path.join(os.environ['HOME'], '.minecraft')
-    if not archive:
-            versions = os.path.sep.join([MCPATH, "versions"])
-            if os.path.exists(versions):
-                archive = os.path.sep.join([versions, "1.8", "1.8.jar"])
+else:
+    MCPATH = os.path.join(os.environ['APPDATA'], '.minecraft')
 
-    blockpath = os.path.sep.join(["assets",
-                                  "minecraft",
-                                  "textures",
-                                  "blocks"])
 
-    with ZipFile(archive) as myzip:
-        for file in myzip.filelist:
-            if file.filename.startswith(blockpath) and file.filename.endswith(".png"):
-                myzip.extract(file.filename, MCPATH)
-                two_paths = os.path.join(MCPATH + os.sep + file.filename)
-                print(two_paths)
-                bpy.data.images.load(two_paths)
+def setup_textures():
+    """ load the resource packs selected in game.  If none are found, load the
+        textures from the games jar.
+    """
+
+    settings = path.join(MCPATH, "options.txt")
+    versions = path.join(MCPATH, "versions")
+    archive = path.sep.join([versions, "1.8", "1.8.jar"])
+
+    if path.exists(versions):
+        load(archive)
+
+    with open(settings) as f:
+        for line in f:
+            if line.startswith("resourcePacks:"):
+                stack = eval(line.split(":")[1])
+                if stack:
+                    rp = path.join(MCPATH, "resourcepacks")
+
+                    if path.exists(rp):
+                        for pack in stack:
+                            pack = path.join(rp, pack)
+                            if path.exists(pack):
+                                load(pack)
+                            elif path.exists(path + ".zip"):
+                                load(pack + ".zip")
+                            else:
+                                print("the pack could not be loaded")
+
+
+def load(archive):
+    """Take the archive file to be loaded into blender."""
+    if path.exists(archive):
+        blockpath = path.sep.join(["assets",
+                                   "minecraft",
+                                   "textures",
+                                   "blocks"])
+        with ZipFile(archive) as myzip:
+            for file in myzip.filelist:
+                fn = file.filename  # Yes, I just did that.
+                if fn.startswith(blockpath) and fn.endswith(".png"):
+                    myzip.extract(file.filename, MCPATH)
+                    two_paths = path.join(MCPATH + os.sep + fn)
+                    bpy.data.images.load(two_paths)
+    else:
+        print("pack not found")
 
 
 def main():
-    load_textures()
+    """Just runs load_textures()."""
+    setup_textures()
 
 if __name__ == '__main__':
     main()
